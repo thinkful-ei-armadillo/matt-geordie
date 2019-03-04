ff1f9cccd33cc9fec375eb53e46dc702f5ef0a49
Geordie Connell <geordie.connell@gmail.com>
2019-03-04 15:09:21 -0500

    authorization set up

--- thingful-server/src/middleware/basic-auth.js
+++ thingful-server/src/middleware/basic-auth.js
@@ -1,14 +1,37 @@
+'use strict';
+
+const AuthService = require('../auth/auth-service');
+
 function requireAuth(req, res, next){
   
   const authToken = req.get('Authorization') || '';
 
+  let bearerToken;
   if(!authToken.toLowerCase().startsWith('bearer ')){
-    return res.status(401).json({error: 'Missing token'})
+    return res.status(401).json({error: 'Missing token'});
+  } else {
+    bearerToken = authToken.slice(7, authToken.length);
   }
 
-  next()
+  const [tokenUserName, tokenPassword] = AuthService.parseBearerToken(bearerToken);
+
+  if(!tokenUserName || !tokenPassword){
+    return res.status(401).json({error : 'Authorization needed'});
+  }
+
+  AuthService.getUserWithUserName(req.app.get('db'), tokenUserName)
+    .then(user => {
+      if(!user || user.password !== tokenPassword){
+        return res.status(401).json({
+          error: 'Authorization needed'
+        });
+      }
+      next();
+    }).catch(next);
+
+  next();
 }
 
 module.exports = {
   requireAuth,
-}
+};
